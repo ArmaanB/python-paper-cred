e4c32d1d4bac468f6928e0b74486ecb6b77367ae
Armaan Bhojwani <me@armaanb.net>
2021-02-24 13:25:41 -0500

    Add parse_uri function

--- paper_cred/qrconvert.py
+++ paper_cred/qrconvert.py
@@ -1,5 +1,17 @@
 import pyzbar.pyzbar as pzybar
 
 
+def parse_uri(uri):
+    colon = uri.split(":", 4)
+    question = colon[3].split(".", 1)[1].split("?")
+    return {
+        "type": colon[1],
+        "version": colon[2],
+        "signature": colon[3].split(".")[0],
+        "pubkey": question[0],
+        "payload": question[1:][0],
+    }
+
+
 def decode(image):
-    return str(pzybar.decode(image)[0].data, "UTF-8")
+    return parse_uri(str(pzybar.decode(image)[0].data, "UTF-8"))
