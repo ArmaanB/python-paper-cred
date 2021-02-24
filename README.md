beee2c3a42b313f91cfcb3ff2f8350a4dcc17887
Armaan Bhojwani <me@armaanb.net>
2021-02-23 20:53:13 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Python paper cred
+
+Python implementation of the [PathCheck paper-first credential spec](https://github.com/Path-Check/paper-cred).
+
+MIT License
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+import os
+
+pwd = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(pwd, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="paper-cred",
+    version="0.0.0",
+    description="PathCheck Verifiable Vaccination Credentials implementation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://pathcheck.org",
+    author="PathCheck foundation",
+    license="MIT",
+    packages=["paper-cred"],
+    entry_points={
+        "console_scripts": ["paper-cred=paper-cred:main"],
+    },
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+    ],
+)
